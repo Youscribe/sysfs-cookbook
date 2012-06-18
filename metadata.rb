f63c2b5a03c9ee37958aeced3b631ae5d9587a5a
Guilhem Lettron <guilhem@lettron.fr>
2012-06-18 12:38:20 +0200

    first working commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Guilhem Lettron"
+maintainer_email "guilhem.lettron@youscribe.com"
+license          "Apache v2.0"
+description      "Configures sysfs parameters"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+supports         "ubuntu"
+supports         "debian"
