783f5df8acc897f461e62efb40e0679e88bb1ded
Edmund Haselwanter <edmund@haselwanter.com>
2012-08-27 13:57:32 +0200

    move to md extension (rendering on github)

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Guilhem Lettron"
 maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Configures sysfs parameters"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 supports         "ubuntu"
 supports         "debian"
