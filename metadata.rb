e2cda150ff6f0de3b37c595b39dee79bdc8b5bd4
Guilhem Lettron <guilhem@lettron.fr>
2013-07-18 15:36:54 +0200

    release v0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Configures sysfs parameters"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 supports         "ubuntu"
 supports         "debian"
