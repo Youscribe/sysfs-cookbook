09f348b95c067ca29b4042065baf633254d3e564
Morley, Jonathan <JMorley@cvent.com>
2016-06-27 15:53:32 -0400

    Support more centos versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,16 +3,16 @@ default['sysfs']['service'] = 'sysfsutils'
 
 default['sysfs']['init_type'] = value_for_platform(
   %w(redhat centos) => {
-    %w(6.0 6.1 6.2 6.3 6.4 6.5 6.6) => 'sysv',
-    'default' => 'systemd'
+    '< 7.0' => 'sysv',
+    :default => 'systemd'
   },
-  'default' => nil
+  :default => nil
 )
 
 default['sysfs']['disable_tuned'] = value_for_platform(
   %w(redhat centos) => {
-    %w(6.0 6.1 6.2 6.3 6.4 6.5 6.6) => false,
-    'default' => true
+    '< 7.0' => false,
+    :default => true
   },
-  'default' => false
+  :default => false
 )
